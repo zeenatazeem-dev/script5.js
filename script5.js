f960a3d1451e84a35daa8db5e5e7a37b0ef05615
Zeenat Azeem <125384808+zeenatazeem-dev@users.noreply.github.com>
2023-04-09 11:17:57 +0500

    check my work please

---
+++ script5.js
@@ -0,0 +1,24 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>JavaScript</title>
+// <link rel="stylesheet" href="./style.css/html">
+// Day 2
+
+// 
+   
+
+// </script>
+// <script type="text/javascript">
+//     alert('Welcome to JavaScript');
+// <script type="text/javascript">
+// alert(5+4);
+var Zakat_persantage =0.025; //2.5%
+var user_input  = Number(prompt("Enter your Amount"));
+var result = 0.025 * user_input;
+alert("your zakat is " + result);
+
+
